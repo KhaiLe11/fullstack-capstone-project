da0835a931d23aff4f5672e3f10caaf1c94967e5
KhaiLe11 <ledinhkhai2009@gmail.com>
2026-07-28 21:03:53 -0400

    initial commit

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -1,3 +1,4 @@
+/*jshint esversion: 8 */
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
@@ -50,8 +51,8 @@ app.use((err, req, res, next) => {
 });
 
 app.get("/",(req,res)=>{
-    res.send("Inside the server")
-})
+    res.send("Inside the server");
+});
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
